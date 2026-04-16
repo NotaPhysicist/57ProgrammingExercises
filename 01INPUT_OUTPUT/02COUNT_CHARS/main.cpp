e13e6ce465668c11fdc385f7dcbf95b4b2e350c3
notaphysicist <notaphysicist@gmail.com>
2026-04-16 10:33:37 +0800

    Change folder structure. Not using git. May be a problem

---
+++ 01INPUT_OUTPUT/02COUNT_CHARS/main.cpp
@@ -0,0 +1,34 @@
+/*
+    Get an input string from the user.
+        "What is the input string: Homer"
+    If the user enters nothing, inform the user that they are retarded 
+    and suggest they try again. 
+    Count the number of chars in the string.
+    Output the result to the user, with the string in the output
+        e.g., "Homer has 5 charcters."
+
+    This challenge reads as a request for simple, procedural code,
+    nothing fancy. 
+ */
+
+#include <iostream>
+#include <string>
+
+void newline() { std::cout << "\n"; }
+void endline() { std::cout << std::endl; }
+void printline(std::string s) { std::cout << s; }
+
+int main()
+{
+    // Get the string from the user
+    std::string input{};
+    newline();
+    printline("COUNT THE LENGTH OF A STRING\n");
+    printline("----------------------------\n");
+    printline("What is the input string: ");
+
+
+    
+    
+}
+
