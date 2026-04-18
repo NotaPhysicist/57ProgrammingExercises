0b47a937d450a30beae71627132eb0dbad5f5d65
notaphysicist <notaphysicist@gmail.com>
2026-04-18 11:45:46 +0800

    Set up the Mad Lib project

---
+++ 01INPUT_OUTPUT/E04MadLib/main.cpp
@@ -0,0 +1,42 @@
+/*
+    Mad Lib. A simple game where you create a story template  with blanks
+    for words.
+
+    PLAN:
+    
+    1. Create a story template (one sentence), with blanks for a noun, verb, 
+       adjective and adverb. 
+    2. Get from the user:
+        A noun:
+        A verb:
+        An adjective
+        An adverb
+    3. Put these words into the story.
+    4. Display the story.
+
+    RELEVENT CONSTRAINTS:
+    
+    1. Use a single output statement (constuct a single string to display)
+    2. Use string substitution or templating if supported. 
+
+    RELEVENT CHALLENGES:
+    
+    1. Make it more complex. Branch into alternate storylines. Add More story. 
+       But this is too tedius. 
+    2. This project is about templating and strings. 
+*/
+
+#include <iostream>
+
+struct wordBlanks
+{
+    std::string nown;
+    std::string verb;
+    std::string adjective;
+    std::string adverb;
+};
+
+int main()
+{
+    
+}
