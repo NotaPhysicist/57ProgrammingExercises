a05a7e54b465abdd08bd2abf3847f6667b586fde
notaphysicist <notaphysicist@gmail.com>
2026-04-26 18:50:12 +0800

    Complete Stage One

--- 02CALCULATIONS/E07AreaRectangleRoom/area.cpp
+++ 02CALCULATIONS/E07AreaRectangleRoom/area.cpp
@@ -6,16 +6,30 @@ void run(FeetToMeterConvert& converter, Console& console)
     constexpr std::string_view promptLfeet{ 
         "What is the length of the room in feet? " };
     constexpr std::string_view promptWfeet{ 
-        "What is the width of the romm in feet " };
+        "What is the width of the room in feet? " };
 
+    // Get dimensions from the user
     double length_ft = inDimension(promptLfeet, console);
     double width_ft = inDimension(promptWfeet, console);
     
-
+    // Calculations
+    double length_m = converter.ftToM(length_ft);
+    double width_m = converter.ftToM(width_ft);
+    double area_ft = length_ft * width_ft;
+    double area_m = length_m + width_m;
     
-    // console.in_Integer("What is the length of the room? ");
-    std::println("{} x {} = {}\n", length_ft, width_ft, length_ft * width_ft);
-    converter.ftToM(23);
+    // Display the result
+    std::println("You entered dimensions of {} feet by {} feet.", 
+        length_ft, width_ft);
+    std::println("These are equivalent to {:.2f} by {:.2f} meters.", 
+        length_m, width_m);
+    std::println("The area is:");
+    std::println("    {} square feet", area_ft);
+    std::println("    {:.2f} square meters\n", area_m);
+
+    // std::println("\nTESTS:");
+    // std::println("{} x {} = {}", length_ft, width_ft, area_ft);
+    // std::println("{} x {} = {}\n", length_m, width_m, area_m);
 }
 
 
@@ -41,5 +55,5 @@ void displayHeader()
 {
     std::string title{ "AREA OF A RECTANGLE ROOM"};
     std::string underline(title.size(), '=');
-    std::println("\n{}\n{}\n", title, underline);    
+    std::println("\n{}\n{}\n{}\n", underline, title, underline);    
 }
