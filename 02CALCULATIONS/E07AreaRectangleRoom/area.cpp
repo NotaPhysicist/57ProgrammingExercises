608e726a8d696d2167976e905996adfca6efc04a
notaphysicist <notaphysicist@gmail.com>
2026-04-27 14:09:13 +0800

    Complete Stage TWo of Area of a Room project. Total project done

--- 02CALCULATIONS/E07AreaRectangleRoom/area.cpp
+++ 02CALCULATIONS/E07AreaRectangleRoom/area.cpp
@@ -9,32 +9,62 @@ void run(FeetToMeterConvert& converter, Console& console)
         "What is the length of the room in feet? " };
     constexpr std::string_view prompt_WidthFeet{ 
         "What is the width of the room in feet? " };
+    constexpr std::string_view prompt_LenghtMeters{ 
+        "What is the length of the room in meters? " };
+    constexpr std::string_view prompt_WidthMeters{ 
+        "What is the width of the room in meters? " };
 
-    char choice = inUnitChoice(prompt_FeetOrMeters, console);
-
-    // Get dimensions from the user
-    double length_ft = inDimension(prompt_LenghtFeet, console);
-    double width_ft = inDimension(prompt_WidthFeet, console);
+    double length_ft{};
+    double length_m{};
+    double width_ft{};
+    double width_m{};
     
+    // Get the choice or units fromt the user
+    char choice = inUnitChoice(prompt_FeetOrMeters, console);
+    switch (choice)
+    {
+        case 'f':  // feet
+            // Get dimensions from the user
+            length_ft = inDimension(prompt_LenghtFeet, console);
+            width_ft = inDimension(prompt_WidthFeet, console);
+            length_m = converter.ftToM(length_ft);
+            width_m = converter.ftToM(width_ft);
+            std::println("\nYou entered dimensions of {} feet by {} feet.", 
+                length_ft, width_ft);
+            std::println("These are equivalent to {:.2f} by {:.2f} meters.", 
+                length_m, width_m);
+            break;
+        case 'm':  // meters
+            length_m = inDimension(prompt_LenghtMeters, console);
+            width_m = inDimension(prompt_WidthMeters, console);
+            length_ft = converter.mToFt(length_m);
+            width_ft = converter.mToFt(width_m);
+            std::println("\nYou entered dimensions of {:.2f} meters by {:.2f} meters.", 
+                length_m, width_m);
+            std::println("These are equivalent to {:.2f} feet by {:.2f} feet.", 
+                length_ft, width_ft);
+            break;
+        default:   // unknown error
+            std::println("Unknown error: program terminated.");
+            return;
+    }
+            
     // Calculations
-    double length_m = converter.ftToM(length_ft);
-    double width_m = converter.ftToM(width_ft);
     double area_ft = length_ft * width_ft;
     double area_m = length_m + width_m;
     
     // Display the result
-    std::println("\nYou entered dimensions of {} feet by {} feet.", 
-        length_ft, width_ft);
-    std::println("These are equivalent to {:.2f} by {:.2f} meters.", 
-        length_m, width_m);
+    
+    // Display area results
     std::println("The area is:");
-    std::println("    {} square feet", area_ft);
+    std::println("    {:.2f} square feet", area_ft);
     std::println("    {:.2f} square meters\n", area_m);
 
-    std::println("\nTESTS:");
-    std::println("{} x {} = {}", length_ft, width_ft, area_ft);
-    std::println("{} x {} = {}", length_m, width_m, area_m);
-    std::println("choice: {}\n", choice);
+    // Display tests while developing. 
+    // std::println("\nTESTS:");
+    // std::println("{} x {} = {}", length_ft, width_ft, area_ft);
+    // std::println("{} x {} = {}", length_m, width_m, area_m);
+    // std::println("choice: {}\n", choice);
 }
 
 
